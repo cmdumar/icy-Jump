d082f57137d4aa42575adea55853bc35874d334b
mohammadumar28 <mohammadumar28@gmail.com>
2020-12-07 15:28:18 +0530

    Design the background

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -3,6 +3,19 @@ import stripe from '../assets/stripe.png';
 import platform from '../assets/platform.png';
 import codey from '../assets/codey.png';
 
+let game;
+let platforms;  // a group of platform objects the player will jump on
+let player; // the actual player controlled sprite
+let cursors;
+let platformCount = 0;
+let emitter;
+let particles;
+let gameOptions = {
+  width: 480,
+  height: 640,
+  gravity: 800,
+};
+
 export default class GameScene extends Phaser.Scene {
   constructor() {
     super('Game');
@@ -18,6 +31,14 @@ export default class GameScene extends Phaser.Scene {
   }
 
   create() {
+    particles = this.add.particles('stripe');
+
+    const graphics = this.add.graphics();
+
+    graphics.fillGradientStyle(0xdadaff, 0x6cfafa, 0xfccaff, 0xdadaff, 1);
+
+    graphics.fillRect(0, 0, gameOptions.width, gameOptions.height);
+
     this.add.image(240, 300, 'logo');
   }
 }
