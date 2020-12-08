06f42edafa87e8d8915229033ec3cbf3ba793178
mohammadumar28 <mohammadumar28@gmail.com>
2020-12-08 18:51:22 +0530

    Add leaderboard to game over

--- src/scenes/EndScene.js
+++ src/scenes/EndScene.js
@@ -5,6 +5,11 @@ import { player } from './PreEndScene';
 const apikey = 'aSvX9oE6ttiscNQe1eCC';
 const base = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/';
 
+const config = {
+  width: 480,
+  height: 640,
+};
+
 export default class EndScene extends Phaser.Scene {
   constructor() {
     super('EndScene');
@@ -23,29 +28,32 @@ export default class EndScene extends Phaser.Scene {
             score,
           }),
         });
-
-        const res = await rawResponse.json();
-
-        console.log('res', res);
+        await rawResponse.json();
       }
 
       const getScores = await fetch(`${base + apikey}/scores/`);
 
       const scores = await getScores.json();
-      console.log(scores);
 
-      scores.result.forEach((i, idx) => {
-        this.add.text(160, (idx * 2) + 100,
-          `${i.user}: ${i.score}`, { fontSize: '18px', fill: '#fff' });
+      let sorted = await Promise.all(await scores.result);
+      sorted = sorted.sort((a, b) => b.score - a.score);
+
+      this.add.text(config.width / 4, 120, 'Leaderboard', {
+        fontSize: '24px',
+        fill: '#f1f1f1',
+      });
+
+      sorted.slice(0, 10).forEach((i, idx) => {
+        this.add.text((config.width / 4), (config.height / 2) - 120 + (idx * 30),
+          `${idx + 1}. ${i.user}: ${i.score}`, { fontSize: '18px', fill: '#fff' });
       });
     })();
-    this.add.text(160, 210,
-      `${player.name}`, { fontSize: '18px', fill: '#fff' });
-    this.add.text(140, 230,
+
+    this.add.text((config.width / 4), 30,
       `Your Score: ${score}`, { fontSize: '18px', fill: '#fff' });
-    this.add.text(180, 250,
+    this.add.text((config.width / 4), 50,
       'Game Over', { fontSize: '15px', fill: '#fff' });
-    this.add.text(152, 270,
+    this.add.text((config.width / 4), 70,
       'Click to Restart', { fontSize: '15px', fill: '#fff' });
 
     this.input.on('pointerup', () => {
