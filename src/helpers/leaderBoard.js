20cc9a0e402625d53b259d38e0678425347f8845
Umar <mohammadumar28@gmail.com>
2020-12-09 17:40:26 +0530

    Fixed saveScores function to use try catch block

--- src/helpers/leaderBoard.js
+++ src/helpers/leaderBoard.js
@@ -3,7 +3,7 @@ import { highScores } from './helpers';
 const leaderBoard = async (base, apikey) => {
   let getScores;
   try {
-    getScores = await fetch(`${base + apikey}/scores/`)
+    getScores = await fetch(`${base + apikey}/scores/`);
     getScores = await getScores.json();
     getScores = await highScores(getScores);
   } catch (err) {
@@ -13,19 +13,24 @@ const leaderBoard = async (base, apikey) => {
 };
 
 const saveScore = async (base, apikey, score, { name }) => {
-  const response = await fetch(`${base + apikey}/scores/`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      user: name,
-      score,
-    }),
-  })
-    .then(res => res.json())
-    .then(r => r.result)
-    .catch(err => err.response.data);
+  let response;
+  try {
+    response = await fetch(`${base + apikey}/scores/`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        user: name,
+        score,
+      }),
+    });
+
+    response = await response.json();
+    response = await response.result;
+  } catch (err) {
+    response = err.response.data;
+  }
 
   return response;
 };
